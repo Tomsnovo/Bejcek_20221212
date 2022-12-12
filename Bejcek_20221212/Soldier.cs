6ac3623f391dc24503662a79639a4b71764fa11a
bejcek.tomas <bejcek.tomas@D20-03>
2022-12-12 12:22:11 +0100

    Add project files.

---
+++ Bejcek_20221212/Soldier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bejcek_20221212
+{
+    internal class Soldier
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int Shield { get; set; }
+        public int Damage { get; set; }
+        public int MaxShield { get; set; }
+
+        public Soldier(string name, int hp, )
+        {
+
+        }
+
+        public void Attack()
+        {
+
+        }
+
+        public void Heal()
+        {
+
+        }
+    }
+}
