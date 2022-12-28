4d659cd0ae0a55c8793145f8cf31abb96a854497
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2022-12-28 20:46:47 +0100

    asd

--- Bejcek_20221212/MainWindow.xaml.cs
+++ Bejcek_20221212/MainWindow.xaml.cs
@@ -20,18 +20,92 @@ namespace Bejcek_20221212
     /// </summary>
     public partial class MainWindow : Window
     {
-        Soldier soldier1 = new Soldier("Pepa");
-        Soldier soldier2 = new Soldier("Franta");
-        
+        Soldier soldier1;
+        Soldier soldier2;
+        int pick = 0;
         public MainWindow()
         {
             InitializeComponent();
-            
+            soldier1 = new Soldier("soldier 1");
+            soldier2 = new Soldier("soldier 2");
+            picked(brdr1, brdr2);
+            Build(soldier1, txtblock1);
+            Build(soldier2, txtblock2);
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Build(Soldier sol, TextBlock txt)
         {
+            txt.Text = " " + sol.Name;
+            txt.Text += "\n HP: " + sol.HP;
+            txt.Text += "\n Shield: " + sol.Shield;
+            txt.Text += "\n Level: " + sol.Level;
 
         }
+
+        public void picked(Border brd, Border Enemybrd)
+        {
+            brd.BorderThickness = new Thickness(1);
+            Enemybrd.BorderThickness = new Thickness(0);
+        }
+        public void Switch()
+        {
+            if (pick == 0)
+            {
+                pick = 1;
+                picked(brdr2, brdr1);
+            }
+            else
+            {
+                pick = 0;
+                picked(brdr1, brdr2);
+            }
+            if (soldier1.lost == true)
+            {
+                MessageBox.Show("soldier 2 vyhral");
+                Application.Current.Shutdown();
+            }
+            else if (soldier2.lost == true)
+            {
+                MessageBox.Show("soldier 1 vyhral");
+                Application.Current.Shutdown();
+            }
+        }
+        private void btnswitch1(object sender, RoutedEventArgs e)
+        {
+            Switch();
+        }
+
+        private void btnattack1(object sender, RoutedEventArgs e)
+        {
+            if (pick == 0)
+            {
+                soldier1.Attack(soldier2);
+            }
+            else
+            {
+                soldier2.Attack(soldier1);
+            }
+            Build(soldier1, txtblock1);
+            Build(soldier2, txtblock2);
+            soldier1.Check(soldier2);
+            soldier2.Check(soldier1);
+            Switch();
+
+        }
+
+        private void btnheal1(object sender, RoutedEventArgs e)
+        {
+            if (pick == 0)
+            {
+                soldier1.Heal();
+            }
+            else
+            {
+                soldier2.Heal();
+            }
+            Build(soldier1, txtblock1);
+            Build(soldier2, txtblock2);
+        }
+
+       
     }
 }
--- Bejcek_20221212/Soldier.cs
+++ Bejcek_20221212/Soldier.cs
@@ -6,36 +6,63 @@ using System.Threading.Tasks;
 
 namespace Bejcek_20221212
 {
-    internal class Soldier
+    public class Soldier
     {
         public string Name { get; set; }
+        public int DMGinRow { get; set; }
         public int HP { get; set; }
         public int MaxHP { get; set; }
+        public int DMG { get; set; }
         public int Shield { get; set; }
-        public int Damage { get; set; }
         public int MaxShield { get; set; }
-
+        public int Level { get; set; }
+        public bool lost = false;
         public Soldier(string name)
         {
             Name = name;
-            HP = 0;
-            MaxHP = 100;  
+            HP = 100;
+            MaxHP = 100;
+            DMG = 25;
             Shield = 100;
-            Damage = 20;
             MaxShield = 100;
+            Level = 1;
         }
-
-        public void Attack(Soldier Enemy)
+        public void Check(Soldier Enemy)
         {
-           if(Enemy.Shield <= 0)
+            if (HP <= 0)
             {
-
+                Enemy.lost = true;
             }
         }
-
+        public void Attack(Soldier Enemy)
+        {
+            if (Enemy.Shield > 0)
+            {
+                Enemy.Shield -= DMG;
+            }
+            else
+            {
+                Enemy.Shield = 0;
+                Enemy.HP -= DMG;
+                DMGinRow += DMG;
+            }
+            LevelUp();
+        }
         public void Heal()
         {
-
+            HP = MaxHP;
+        }
+        public void LevelUp()
+        {
+            if (DMGinRow >= 100)
+            {
+                DMGinRow = 0;
+                MaxHP += 20;
+                MaxShield += 20;
+                Shield = MaxShield;
+                Heal();
+                Level++;
+            }
         }
     }
 }
